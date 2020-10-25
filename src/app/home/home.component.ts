d7c3f772d98a5ebc37ea61ac954063c96827e4c5
MoleleMichael <molelemichael1997@gmail.com>
2020-10-25 16:49:45 -0700

    2 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  Productimage: string;
+  Productname: string;
+  position: number;
+  quantity: number;
+  
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, Productimage: "https://www.powerplanetonline.com/cdnassets/raton_gaming_logitech_g102_prodigy_usb_negro_01_l.jpg", Productname: "Mouse" ,quantity: 14},
+];
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,4 +24,6 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+   displayedColumns: string[] = ['position', 'Productimage', 'Productname', 'quantity'];
+  dataSource = ELEMENT_DATA;
 }
