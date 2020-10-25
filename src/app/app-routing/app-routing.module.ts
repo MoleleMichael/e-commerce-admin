45b9ab382ca176aa85f7b1aff902093c1fb2d547
MoleleMichael <molelemichael1997@gmail.com>
2020-10-25 13:22:03 -0700

    2 files modified, 7 added

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from '../register/register.component';
 import { LoginComponent } from '../login/login.component';
+import { AddingComponent } from '../adding/adding.component';
+import { HomeComponent } from '../home/home.component';
 
 const approuting: Routes = [
   {path:'LoginComponent', component: LoginComponent},
-  {path:'', component: RegisterComponent}
+  {path:'register', component: RegisterComponent},
+  {path:'', component: HomeComponent},
+  {path: 'adding', component: AddingComponent}
   ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule,  ReactiveFormsModule } from "@angular/forms";
 
+//********* Angular Materials *************\\
 import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
+import {MatDividerModule} from '@angular/material/divider';
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
@@ -13,7 +15,9 @@ import { RegisterComponent } from "./register/register.component";
 import { LoginComponent } from "./login/login.component";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
 import { RouterModule, Routes } from "@angular/router";
- 
+import { AddingComponent } from './adding/adding.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   imports: [
@@ -24,13 +28,16 @@ import { RouterModule, Routes } from "@angular/router";
     MatFormFieldModule,
     ReactiveFormsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatDividerModule
   ],
   declarations: [
     AppComponent,
     HelloComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    AddingComponent,
+    HomeComponent
   ],
   bootstrap: [AppComponent]
 })
